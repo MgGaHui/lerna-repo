a93e974f741b255825f31a4572b9e9d32b6fff82
xiaomei <844980183@qq.com>
2020-09-06 22:48:33 +0800

    主项目修改引入包组件

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <package-test></package-test>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PackageTest from '../packages/package-test'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'package-test':PackageTest
   }
 }
 </script>
